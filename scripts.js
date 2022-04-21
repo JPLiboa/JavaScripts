c0fb47894318d6f7fb46f8039e49e3b1d5bf2910
Juan Liboa <jpliboa@teco.com.ar>
2022-04-21 14:47:47 -0300

    proceso giftcard

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+let giftCardTotal= 10000
+let giftCard= parseInt( prompt ("Ingrese el monto de la GiftCard"))
+
+while (isNaN(giftCard)){
+    giftCard = parseInt(prompt('Monto no válido, ingreselo nuevamente'));
+}
+let Cenas= 5000
+let Indumentaria= 50000
+let Celulares= 5000
+
+let giftCardTipo= parseInt( prompt ("Ingrese el tipo de GiftCard: Cenas - Indumentaria - Celulares"))
+
+
+ 
+
+if (Cenas==true){
+    prompt("Ingrese el e-mail del beneficiado")
+
+}else{
+    alert ("opcion no valida")
+}
+
+alert("¡¡Muchas gracias por elegirnos!!")
+
+
+/*
 let giftCardFor = 10000
 
 const items = [
@@ -5,14 +31,14 @@ const items = [
 ]
 
 console.log(items.length)
-/*version for*/
+/*version for
 for (let i=0;i<items.length;i++){
     giftCardFor-=items[i]    
 }
 
 console.log(giftCardFor)
 
-/*version while*/
+/*version while
 let giftCardWhile = 5000
 let n=0
 while (n<items.length){
@@ -23,12 +49,12 @@ while (n<items.length){
 
 console.log(giftCardWhile)
 
-/*Version DoWhile*/
+/*Version DoWhile
 let giftCardDoWhile = 2000
-let s=0
+let s=
 do{
     giftCardDoWhile-=items[s]
     s++
 } while (s<items.length)
     
-console.log(giftCardDoWhile)
+console.log(giftCardDoWhile)*/
